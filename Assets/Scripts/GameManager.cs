d578d95de676879b1da9e23d720f6c83a238d6b1
Xing Ye <tanxingye23@gmail.com>
2023-11-04 00:48:00 +0800

    Added Changes
    Added Changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int Score = 0;
+    public Text scoretext;
+    public void UpdateScore(int value)
+    {
+        Score += value;
+        scoretext.text = "Score : " + Score;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
+    public Rigidbody rb;
+    GameManager GM;
     void Start()
     {
-        
+        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            GM.UpdateScore(1);
+        }
     }
 }
